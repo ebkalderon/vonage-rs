c8775af9228a87d64ac5acda703c955a80ed604e
Eyal Kalderon <ebkalderon@gmail.com>
2020-09-17 12:15:30 +0800

    Generate signatures from T: Serialize, add unit tests
    In the process of making these changes, we now also automatically drop
    the `sig` field, if present in the query params, to ensure it's not
    being factored into the signature computation.

--- src/sig.rs
+++ src/sig.rs
@@ -2,11 +2,21 @@
 
 use std::borrow::Cow;
 use std::collections::BTreeMap;
-use std::fmt::{self, Debug, Formatter};
+use std::fmt::{self, Debug, Formatter, Write};
 
 use hmac::{digest::Digest, Hmac, Mac, NewMac};
+use serde::Serialize;
 
-pub(crate) struct Hash(pub String);
+#[derive(Serialize)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
+pub(crate) struct SignatureHash(pub String);
+
+#[cfg(test)]
+impl<T: AsRef<str>> PartialEq<T> for SignatureHash {
+    fn eq(&self, other: &T) -> bool {
+        self.0.eq(other.as_ref())
+    }
+}
 
 /// A cryptographic signature used for signing SMS message requests.
 #[derive(Clone)]
@@ -47,39 +57,36 @@ impl Signature {
         }
     }
 
-    pub(crate) fn generate_from<'a, I>(&self, query_params: I) -> Hash
-    where
-        I: IntoIterator<Item = (&'a str, &'a str)>,
-    {
-        let sanitized = to_sanitized_str(query_params);
+    pub(crate) fn sign<T: Serialize>(&self, query_params: T) -> SignatureHash {
+        let payload = to_payload_str(query_params);
         let hash = match &self.method {
             SignatureMethod::Md5Hash => {
-                let concat = format!("{}{}", sanitized, self.secret);
-                format!("{:x}", md5::Md5::new().chain(concat).finalize())
+                let hasher = md5::Md5::new().chain(payload).chain(self.secret.as_bytes());
+                format!("{:x}", hasher.finalize())
             }
             SignatureMethod::Md5Hmac => {
                 let mut hmac = Hmac::<md5::Md5>::new_varkey(self.secret.as_bytes()).unwrap();
-                hmac.update(sanitized.as_bytes());
+                hmac.update(payload.as_bytes());
                 format!("{:x}", hmac.finalize().into_bytes())
             }
             SignatureMethod::Sha1Hmac => {
                 let mut hmac = Hmac::<sha1::Sha1>::new_varkey(self.secret.as_bytes()).unwrap();
-                hmac.update(sanitized.as_bytes());
+                hmac.update(payload.as_bytes());
                 format!("{:x}", hmac.finalize().into_bytes())
             }
             SignatureMethod::Sha256Hmac => {
                 let mut hmac = Hmac::<sha2::Sha256>::new_varkey(self.secret.as_bytes()).unwrap();
-                hmac.update(sanitized.as_bytes());
+                hmac.update(payload.as_bytes());
                 format!("{:x}", hmac.finalize().into_bytes())
             }
             SignatureMethod::Sha512Hmac => {
                 let mut hmac = Hmac::<sha2::Sha512>::new_varkey(self.secret.as_bytes()).unwrap();
-                hmac.update(sanitized.as_bytes());
+                hmac.update(payload.as_bytes());
                 format!("{:x}", hmac.finalize().into_bytes())
             }
         };
 
-        Hash(hash)
+        SignatureHash(hash)
     }
 }
 
@@ -92,12 +99,16 @@ impl Debug for Signature {
     }
 }
 
-fn to_sanitized_str<'a>(query_params: impl IntoIterator<Item = (&'a str, &'a str)>) -> String {
-    let sorted: BTreeMap<_, _> = query_params.into_iter().collect();
-    sorted
-        .into_iter()
-        .map(|(k, v)| format!("&{}={}", k, v.replace('&', "_").replace('=', "_")))
-        .collect()
+#[inline]
+fn to_payload_str<T: Serialize>(query_params: T) -> String {
+    let encoded = serde_urlencoded::to_string(query_params).expect("query_params must be map-like");
+    let mut sorted: BTreeMap<&str, &str> = serde_urlencoded::from_str(&encoded).unwrap();
+    sorted.remove("sig");
+    let buf = String::with_capacity(encoded.len()); // Reasonable heuristic, given likely similar lengths.
+    sorted.into_iter().fold(buf, |mut acc, (k, v)| {
+        write!(acc, "&{}={}", k, v.replace(&['&', '='][..], "_")).unwrap();
+        acc
+    })
 }
 
 /// A list of supported SMS signature methods.
@@ -132,6 +143,52 @@ mod tests {
     #[test]
     fn default_sig_method_is_md5_hash() {
         assert_eq!(SignatureMethod::default(), SignatureMethod::Md5Hash);
-        assert_eq!(Signature::new("hello").method, SignatureMethod::Md5Hash);
+        assert_eq!(Signature::new("secret").method, SignatureMethod::Md5Hash);
+    }
+
+    #[test]
+    fn strips_sig_field_if_present() {
+        #[derive(Serialize)]
+        struct Params {
+            sig: &'static str,
+        }
+
+        let hash = Signature::new("secret").sign(Params { sig: "hello" });
+        assert_eq!(hash, "5ebe2294ecd0e0f08eab7690d2a6ee69");
+    }
+
+    #[test]
+    fn generates_md5_signature() {
+        #[derive(Serialize)]
+        struct Params {
+            from: &'static str,
+        }
+
+        let hash = Signature::new("secret").sign(());
+        assert_eq!(hash, "5ebe2294ecd0e0f08eab7690d2a6ee69");
+
+        let hash = Signature::new("secret").sign(Params { from: "VONAGE" });
+        assert_eq!(hash, "129d3e7ca8b1acf36cb5ccb92dfec55c");
+    }
+
+    #[test]
+    fn generates_sha1_signature() {
+        let hash = Signature::with_method(SignatureMethod::Sha1Hmac, "secret").sign(());
+        assert_eq!(hash, "25af6174a0fcecc4d346680a72b7ce644b9a88e8");
+    }
+
+    #[test]
+    fn generates_sha256_signature() {
+        let hash = Signature::with_method(SignatureMethod::Sha256Hmac, "secret").sign(());
+        assert_eq!(
+            hash,
+            "f9e66e179b6747ae54108f82f8ade8b3c25d76fd30afde6c395822c530196169"
+        );
+    }
+
+    #[test]
+    fn generates_sha512_signature() {
+        let hash = Signature::with_method(SignatureMethod::Sha512Hmac, "secret").sign(());
+        assert_eq!(hash, "b0e9650c5faf9cd8ae02276671545424104589b3656731ec193b25d01b07561c27637c2d4d68389d6cf5007a8632c26ec89ba80a01c77a6cdd389ec28db43901");
     }
 }
